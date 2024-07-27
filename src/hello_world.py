99af6afde2933b2792f1552a6b559d1e14c65b21
Andrew Goessling <agoessling@gmail.com>
2024-07-26 23:07:50 -0700

    Add C++ and python sources.

---
+++ src/hello_world.py
@@ -0,0 +1,8 @@
+# Unused import
+import os
+
+# Lint violation, which is not auto-fixable.
+# When running with `--fix` this one should be reported and lint should exit 1.
+print("{".format("something"))
+
+print("Hello World!")
